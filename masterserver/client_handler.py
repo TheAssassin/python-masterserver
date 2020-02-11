1b8545a159039924701c297a639e16485e601ce0
TheAssassin <theassassin@assassinate-you.net>
2020-02-11 01:06:23 +0100

    Break up client handler

--- masterserver/client_handler.py
+++ masterserver/client_handler.py
@@ -17,66 +17,74 @@ class ClientHandler:
         self._writer: StreamWriter = writer
         self._master_server = master_server
 
-    async def handle(self):
-        try:
+        self._client_data = self._writer.get_extra_info("peername")
 
-            client = self._writer.get_extra_info("peername")
+    async def _handle_update_command(self):
+        lines = [
+            "setversion 160 230",
+            "clearservers",
+        ]
 
-            self._logger.info("client connected: %r", client)
+        server: RedEclipseServer
+        for server in self._master_server.servers:
+            lines.append("addserver %s" % server.addserver_line())
+
+        response = "\n".join(lines)
+        response += "\n"
+
+        self._writer.write(response.encode("cube2"))
+
+        self._logger.info("closing connection from client %r", client)
+
+    async def _handle_server(self, first_command: str):
+        re_server = None
+
+        command = first_command
+
+        while True:
+            if not command:
+                # the connection will be cleaned up by the caller, therefore we just have to clean up the server entry
+                self._logger.warning("Lost connection to client %r, closing", self._client_data)
+
+                if re_server is not None:
+                    if await self._master_server.remove_server(re_server):
+                        self._logger.info("removed server %r", re_server)
+
+                return
+
+            match = re.match(r'server ([0-9]+) ([^\s]+) ([0-9]+) "([^"]*)" ([0-9]+) "([^"]*)"', command)
+
+            if not match:
+                raise ValueError("Invalid server command", command)
+
+            host, _ = self._writer.get_extra_info("peername")
+            port, serverip, version, _, _, branch = match.groups()
+
+            self._logger.info("Received registration request for server %s:%d", host, int(port))
+
+            re_server = await self._master_server.register_server(host, serverip, int(port), branch)
 
             command = (await self._reader.readline()).decode().rstrip("\n")
 
-            if command == "update":
-                lines = [
-                    "setversion 160 230",
-                    "clearservers",
-                ]
+    async def handle(self):
+        try:
+            self._logger.info("client connected: %r", self._client_data)
 
-                server: RedEclipseServer
-                for server in self._master_server.servers:
-                    lines.append("addserver %s" % server.addserver_line())
+            first_command = (await self._reader.readline()).decode().rstrip("\n")
 
-                response = "\n".join(lines)
-                response += "\n"
+            if first_command == "update":
+                await self._handle_update_command()
 
-                self._writer.write(response.encode("cube2"))
-
-                self._logger.info("closing connection from client %r", client)
-                return
-
-            elif command.startswith("server "):
-                re_server = None
-
-                while True:
-                    if not command:
-                        self._logger.warning("Lost connection to client %r, closing", client)
-                        self._writer.close()
-                        await self._writer.wait_closed()
-
-                        if re_server is not None:
-                            if await self._master_server.remove_server(re_server):
-                                self._logger.info("removed server %r", re_server)
-
-                        return
-
-                    match = re.match(r'server ([0-9]+) ([^\s]+) ([0-9]+) "([^"]*)" ([0-9]+) "([^"]*)"', command)
-
-                    if not match:
-                        raise ValueError("Invalid server command", command)
-
-                    host, _ = self._writer.get_extra_info("peername")
-                    port, serverip, version, _, _, branch = match.groups()
-
-                    self._logger.info("Received registration request for server %s:%d", host, int(port))
-
-                    re_server = await self._master_server.register_server(host, serverip, int(port), branch)
-
-                    command = (await self._reader.readline()).decode().rstrip("\n")
+            # server try to keep up their TCP connection
+            # the reason upstream is probably rate limiting, but here it's planned to implement rate limiting by
+            # limiting the amount of servers in the server list rather than closing new connections
+            # in any case we can run the specific handler from here, the try-finally will clean up the connection
+            elif first_command.startswith("server "):
+                await self._handle_server(first_command)
 
             else:
-                self._logger.error("unknown command %s from client %r, closing connection", command, client)
+                self._logger.error("unknown command %s from client %r, closing connection", first_command, client)
 
         finally:
             self._writer.close()
             await self._writer.wait_closed()
-            return
